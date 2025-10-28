b0c6cb0bf19582f290c38fad8a322a6fe0eeebe2
nandamiftahul <nanda.miftahul91@gmail.com>
2025-10-28 14:31:58 +0700

    change wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from app import create_app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
+    # hanya untuk development (bukan Railway)
     app.run(host="0.0.0.0", port=8082, debug=True)
-else:
-    app = create_app()
