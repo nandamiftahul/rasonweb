e4b36762789cf16604e965ef03147664210eeff1
nandamiftahul <nanda.miftahul91@gmail.com>
2025-10-28 14:29:48 +0700

    change wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,7 @@
-import os, sys
-from pathlib import Path
-
-# Pastikan project root /app dikenali oleh Python
-BASE_DIR = Path(__file__).resolve().parent
-os.chdir(BASE_DIR)
-if str(BASE_DIR) not in sys.path:
-    sys.path.insert(0, str(BASE_DIR))
-
-# Debug print (boleh hapus setelah berhasil)
-print("✅ WSGI running from:", os.getcwd())
-print("✅ sys.path includes:", sys.path)
-
-# Import create_app
 from app import create_app
 
-app = create_app()
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=8082, debug=True)
+else:
+    app = create_app()
