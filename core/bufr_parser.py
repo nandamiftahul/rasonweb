4db5503522e9207da9c6200c38af4c3a7f6d29c6
Nanda Miftahul K <nanda.miftahul91@gmail.com>
2025-10-29 19:39:34 +0700

    Update bufr_parser.py

--- core/bufr_parser.py
+++ core/bufr_parser.py
@@ -192,8 +192,8 @@ def parse_bufr(decoded_text, site="default"):
                 current[key] = val
                 matched_level = True
                 break
-        print(meta) 
-        print(current) 
+        #print(meta) 
+        #print(current) 
         if matched_level:
             continue
 
